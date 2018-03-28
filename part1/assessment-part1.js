d10c2429c2abbea8c66e8de6da0224cec8a2cb24
Zac Cericola <zcericola@gmail.com>
2018-03-28 16:41:16 -0500

    intermediate js assessment

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -134,8 +134,8 @@ mustang.drive();
 
 
 // CODE HERE...
-String.prototype.grammarPolice = function(str){
-  str = str.toLowerCase().split(" ");
+String.prototype.grammarPolice = function(){
+  str = this.toLowerCase().split(" ");
   for (var i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
